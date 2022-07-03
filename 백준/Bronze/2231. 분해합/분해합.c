1894d6b88a53a638b72ef10821b71db06cb7f32e
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-07-03 15:24:58 +0900

    Update 분해합.c

--- 백준/Bronze/2231. 분해합/분해합.c
+++ 백준/Bronze/2231. 분해합/분해합.c
@@ -2,15 +2,15 @@
 int DSum(int);
 int main(void)
 {
-    int n, tmp, len, i, rst=0;
+    int n, tmp, len, i, rst=0;      //생성자가 없는 경우 0을 출력하기 위해 rst를 0으로 초기화
     scanf("%d", &n);
     tmp=n;
-    for(len=0;tmp>0;len++)
+    for(len=0;tmp>0;len++)      //입력된 수의 자릿수 len 구하기
         tmp/=10;
     
-    for(i=n-9*len;i<n;i++)
+    for(i=n-9*len;i<n;i++)      //자연수n의 생성자 최소치는 n에서 각 자리 수가 9일 경우를 뺀 수치
     {
-        if(DSum(i)==n){
+        if(DSum(i)==n){         //최소치부터 확인해서 분해합이 n과 동일할 경우
             rst=i;
             break;
         }
@@ -18,11 +18,11 @@ int main(void)
     printf("%d \n", rst);
     return 0;
 }
-int DSum(int num)    //분해합 구하는 함수
+int DSum(int num)   //분해합 구하는 함수
 {
     int sum=0;
-    sum+=num;
-    while(num>0)
+    sum+=num;       //자기 자신을 더하기
+    while(num>0)    //각 자리 수 더하기
     {
         sum+=num%10;
         num/=10;
