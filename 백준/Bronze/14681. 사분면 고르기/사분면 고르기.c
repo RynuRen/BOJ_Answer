411b818faab62ec616de8dd7880fa85c7e9face8
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-07-03 13:33:22 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a>0){
+        if(b>0)
+            printf("1");
+        else
+            printf("4");
+    }        
+    else{
+        if(b>0)
+            printf("2");
+        else
+            printf("3");
+    }
+    return 0;
+}
