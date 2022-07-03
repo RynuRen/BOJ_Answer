6b69ba4189890a32e3cd92bebc3f5ddbbd987fcc
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-07-03 13:45:47 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+    int a, b, c;
+    int mult, i, j, cnt;
+    int arr[10];
+    scanf("%d %d %d", &a, &b, &c);
+    mult=a*b*c;
+    for(i=0;i<10;i++)
+    {
+        arr[i]=mult%10;
+        if(mult==0)
+            arr[i]=-1;
+        mult/=10;
+    }
+    for(j=0;j<10;j++)
+    {
+        cnt=0;
+        for(i=0;i<10;i++)
+        {            
+            if(arr[i]==j)
+                cnt++;            
+        }
+        printf("%d \n", cnt);
+    }
+    return 0;
+}
