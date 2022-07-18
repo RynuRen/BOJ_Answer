0cf54e3fc613fcf14a9c8cb87ca1a1db9643136a
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-07-18 09:57:21 +0900

    Update 이항 계수 1.c

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.c
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.c
@@ -3,7 +3,7 @@ int factorial(int);
 int main(void)
 {
     //이항계수:N개의 원소집합에서 K개의 원소를 순서없이 선택하는 경우의 수
-    //answer=N!/(N-K)!/K!
+    //nＣk=N!/(N-K)!/K!
     int n, k;
     scanf("%d %d", &n,&k);
     printf("%d\n", factorial(n)/factorial(n-k)/factorial(k));
