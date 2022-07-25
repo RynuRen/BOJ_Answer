6a93825a4f4e267c4a7951df143dab842a33069b
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-07-25 15:02:20 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5016 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.c
+++ 백준/Silver/1904. 01타일/01타일.c
@@ -5,15 +5,14 @@ int tile(int);
 int main(void)
 {
     int n;
-    t[1]=1;
-    t[2]=2;
     scanf("%d", &n);
     printf("%d\n", tile(n));
     return 0;
 }
 int tile(int n)
 {
-    if(t[n]==0)
-        t[n]=(tile(n-1)+tile(n-2))%15746;
+    t[1]=1, t[2]=2;
+    for(int i=3;i<=n;i++)
+        t[i]=(t[i-1]+t[i-2])%15746;
     return t[n];
 }
