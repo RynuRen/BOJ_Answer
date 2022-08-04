7c10b5cb31d5cf6b9dc87adb13fa6d37ec5d0966
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-08-04 12:16:19 +0900

    [Silver II] Title: 마인크래프트, Time: 148 ms, Memory: 2096 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.c
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.c
@@ -3,42 +3,35 @@
 
 int main(void)
 {
-    int** ground;
+    int* ground;
     int n, m, b, time, inv;
     int min=256, max=0;
     int min_time=256*500*500*2;
     int height=256;
     scanf("%d %d %d", &n, &m, &b);
-    ground=(int**)malloc(n*sizeof(int*));
-    for(int i=0;i<n;i++){
-        ground[i]=(int*)malloc(m*sizeof(int));
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            scanf("%d", &ground[i][j]);
-            if(ground[i][j]<min){
-                min=ground[i][j];
-            }
-            if(ground[i][j]>max){
-                max=ground[i][j];
-            }
+    ground=(int*)malloc(n*m*sizeof(int));
+    for(int i=0;i<n*m;i++){
+        scanf("%d", &ground[i]);
+        if(ground[i]<min){
+            min=ground[i];
+        }
+        if(ground[i]>max){
+            max=ground[i];
         }
     }
     for(int k=max;k>=min;k--){
-        inv=b;
+        inv=b;  //환경 초기화
         time=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(ground[i][j]>k){
-                    //블록을 제거 인벤에 넣기
-                    time+=2*(ground[i][j]-k);
-                    inv+=ground[i][j]-k;
-                }
-                else if(ground[i][j]<k){
-                    //인벤에서 꺼내 블록을 놓기
-                    time+=k-ground[i][j];
-                    inv-=k-ground[i][j];
-                }
+        for(int i=0;i<n*m;i++){
+            if(ground[i]>k){
+                //블록을 제거 인벤에 넣기
+                time+=2*(ground[i]-k);
+                inv+=ground[i]-k;
+            }
+            else if(ground[i]<k){
+                //인벤에서 꺼내 블록을 놓기
+                time+=k-ground[i];
+                inv-=k-ground[i];
             }
         }
         //인벤이 -가 되는 경우 제외
@@ -50,9 +43,6 @@ int main(void)
         }
     }
     printf("%d %d\n", min_time, height);
-    for(int i=0;i<n;i++){
-        free(ground[i]);
-    }
     free(ground);
     return 0;
 }
