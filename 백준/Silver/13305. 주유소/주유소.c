87c55583c5779e13506ecaf4d590e0aea92a6d82
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-08-30 10:31:39 +0900

    [Silver IV] Title: 주유소, Time: 36 ms, Memory: 1384 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.c
+++ 백준/Silver/13305. 주유소/주유소.c
@@ -2,40 +2,19 @@
 
 int main(void)
 {
-    int n, i, flag=0;
-    //10^9 * 10^9 결과 int 범위를 넘지만 long long int 범위 내
-    long long price=0;  //기름값
-    //10^9 * (10^5-1) 결과 int 범위를 넘지만 long long int 범위 내
-    long long dis[100001]={0,}; //거리 누적합 배열
-    int oil[100001];
+    int n, oil, min=0;
+    long long price=0;
+    int dis[100000];
     scanf("%d", &n);
-    for(i=1;i<n;i++){
-        int tmp;
-        scanf("%d", &tmp);
-        dis[i]=dis[i-1]+tmp;
+    for(int i=0;i<n-1;i++){
+        scanf("%d", &dis[i]);
     }
-    for(i=0;i<n;i++){
-        scanf("%d", &oil[i]);
-    }
-    i=0;
-    while(i<n){
-        for(int j=i+1;j<n;j++){
-            //지금 도시 이후에 더 싼 주유소가 있다면
-            //가장 가까운 그 도시까지 갈 기름만 넣음
-            if(oil[i]>oil[j]){
-                price+=oil[i]*(dis[j]-dis[i]);
-                flag=1;
-                i=j;
-                break;
-            }
+    for(int i=0;i<n-1;i++){ //n-1번째(목적지)의 기름값은 필요없는 정보
+        scanf("%d", &oil);
+        if(i==0 || min>oil){
+            min=oil; //최저값 갱신
         }
-        //목적지까지 더 싼 주유소가 없다면
-        //지금 도시에서 목적지까지 갈 기름을 모두 넣음
-        if(!flag){
-            price+=oil[i]*(dis[n-1]-dis[i]);
-            break;
-        }
-        flag=0;
+        price+=(long long)min*dis[i]; //연산 과정에 int 범위 초과 방지
     }
     printf("%lld\n", price);
     return 0;
