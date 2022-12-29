2a6b16c7878918f6b50f01a6a64e6d0436fa33a3
RynuRen <46422392+RynuRen@users.noreply.github.com>
2022-12-29 15:16:16 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,12 @@
+paper = [[0 for _ in range(100)] for _ in range(100)]
+
+result = 0
+num = int(input())
+for _ in range(num):
+    x, y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            if paper[i][j] != 1:
+                paper[i][j] = 1
+                result += 1
+print(result)
