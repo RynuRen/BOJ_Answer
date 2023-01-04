0ebf786e4c86714728c43703e234559f405aff65
RynuRen <46422392+RynuRen@users.noreply.github.com>
2023-01-04 22:43:07 +0900

    [Silver IV] Title: 한수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -3,7 +3,6 @@ cnt = 0
 for num in range(1, n+1):
     if num < 100:
         cnt += 1
-    else:
-        if num//100 - (num//10)%10 == (num//10)%10 - num%10:
-            cnt += 1
+    elif num//100 - (num//10)%10 == (num//10)%10 - num%10:
+        cnt += 1
 print(cnt)
