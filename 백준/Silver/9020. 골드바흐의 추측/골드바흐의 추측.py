dc8f0e08426a5f24ce451b21ccdea744348c9ef6
RynuRen <46422392+RynuRen@users.noreply.github.com>
2023-01-10 16:11:01 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 504 ms, Memory: 30620 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,12 +1,12 @@
-non_pn = [0 for _ in range(10000+1)]
-non_pn[0] = non_pn[1] = 1
+non_pn = [False for _ in range(10000+1)]
+non_pn[0] = non_pn[1] = True
 
 def pn_chekcer(n):
     for i in range(2, n+1):
-        if non_pn[i] == 1:
+        if non_pn[i] == True:
             continue
         for j in range(2, n//i+1):
-            non_pn[i*j] = 1
+            non_pn[i*j] = True
 
 pn_chekcer(10000)
 
@@ -14,7 +14,7 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     for i in range(n//2, 1, -1):
-        if non_pn[i] != 1:
-            if non_pn[n - i] != 1:
+        if non_pn[i] != True:
+            if non_pn[n - i] != True:
                 print(i, n-i, sep=' ')
                 break
