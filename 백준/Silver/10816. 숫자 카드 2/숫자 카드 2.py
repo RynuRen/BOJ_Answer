817ffff871f00fc6fb098275958d2f974e9339f4
RynuRen <46422392+RynuRen@users.noreply.github.com>
2023-01-17 13:09:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1676 ms, Memory: 118724 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,33 +1,12 @@
-import sys
+import sys, bisect
 input = sys.stdin.readline
-
-def bin_seek_low(tar, end):
-    start = 0
-    while start < end:
-        mid = (start + end) // 2
-        if N[mid] >= tar:
-            end = mid
-        else:
-            start = mid + 1
-    return start
-
-def bin_seek_up(tar, end):
-    start = 0
-    while start < end:
-        mid = (start + end) // 2
-        if N[mid] > tar:
-            end = mid
-        else:
-            start = mid + 1
-    return start
 
 n = int(input())
 N = list(map(int, input().split()))
-
 N.sort()
 
 m = int(input())
 M = list(map(int, input().split()))
 
 for i in M:
-    print(bin_seek_up(i, n) - bin_seek_low(i, n), end=' ')
+    print(bisect.bisect(N, i) - bisect.bisect_left(N, i), end=' ')
