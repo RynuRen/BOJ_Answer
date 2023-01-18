fd256cdcff4d9f22d0a8b07b9c4809ea4a2a454e
RynuRen <46422392+RynuRen@users.noreply.github.com>
2023-01-18 23:21:13 +0900

    [Silver IV] Title: 큐 2, Time: 1528 ms, Memory: 175160 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+queue = deque()
+n = int(input())
+for _ in range(n):
+    order = input().split()
+    if order[0] == 'push':
+        queue.append(order[1])
+    elif order[0] == 'pop':
+        if not queue:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif order[0] == 'size':
+        print(len(queue))
+    elif order[0] == 'empty':
+        if not queue:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'front':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+    elif order[0] == 'back':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
