f9241039461a4642d4df962068e5c4e378f50569
RynuRen <46422392+RynuRen@users.noreply.github.com>
2023-01-21 16:43:41 +0900

    [Silver IV] Title: 국영수, Time: 380 ms, Memory: 63296 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+std = []
+
+for _ in range(n):
+    raw = input().split()
+    scores = list(map(int, raw[1:]))
+    std.append([raw[0]]+scores)
+
+std.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+print(*[i[0] for i in std], sep='\n')
