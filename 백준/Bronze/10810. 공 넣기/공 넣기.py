23ec65a10c3ac81990333d6c178a0c8bfb09c288
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-02-26 21:39:12 +0900

    [Bronze III] Title: 공 넣기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -2,6 +2,6 @@ n, m = map(int, input().split())
 burket = [0] * n
 for _ in range(m):
     i, j, k = map(int, input().split())
-    for num in range(i-1, j):
-        burket[num] = k
+    for num in range(i, j+1):
+        burket[num-1] = k
 print(*burket)
