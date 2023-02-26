5806d45492d1096a8362cff78370fe46d09efce1
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-02-26 22:38:20 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,11 +1,7 @@
 n, m = map(int, input().split())
-bk = []
-for i in range(n):
-    bk.append(i+1)
+bk = [i+1 for i in range(n)]
+
 for _ in range(m):
     i, j = map(int, input().split())
-    if i == 1:
-        bk[:j] = bk[j-1::-1]
-    else:
-        bk[i-1:j] = bk[j-1:i-2:-1]
+    bk[i-1:j] = bk[i-1:j][::-1]
 print(*bk)
