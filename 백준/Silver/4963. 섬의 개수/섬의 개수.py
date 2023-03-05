b940ec68d4a6b39d5d8e0e15ef22251adb459668
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-03-05 21:19:41 +0900

    [Silver II] Title: 섬의 개수, Time: 80 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(x, y):
+    dx = [0, 1, 1, 1, 0, -1, -1, -1]
+    dy = [1, 1, 0, -1, -1, -1, 0, 1]
+
+    imap[x][y] = 0
+    q = deque()
+    q.append([x, y])
+    while q:
+        a, b = q.popleft()
+        for i in range(8):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < h and 0 <= ny < w and imap[nx][ny] == 1:
+                imap[nx][ny] = 0
+                q.append([nx, ny])
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break    
+    imap = []
+    island = 0
+    for _ in range(h):
+        imap.append(list(map(int, input().split())))
+
+    for i in range(h):
+        for j in range(w):
+            if imap[i][j] == 1:
+                bfs(i, j)
+                island += 1
+    print(island)
