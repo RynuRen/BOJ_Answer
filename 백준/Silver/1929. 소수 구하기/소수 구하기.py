f29f40816978f91c324d48cf185c5c6c5528fa8b
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-03-18 21:13:12 +0900

    [Silver III] Title: 소수 구하기, Time: 280 ms, Memory: 38928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -3,7 +3,7 @@ m, n = map(int, input().split())
 pn = [True] * (n+1)
 
 for i in range(2, int(n**0.5)+1):
-    if pn:
+    if pn[i]:
         for j in range(i+i, n+1, i):
             pn[j] = False
             
