6f75e91a75f364db1c062957af1aacf27c29c4fd
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-05-08 11:59:53 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,10 @@
+grade = {"A+":4.5, "A0":4.0, "B+":3.5, "B0":3.0, "C+":2.5, "C0":2.0, "D+":1.5, "D0":1.0, "F":0.0}
+total = 0
+cnt = 0
+for i in range(20):
+    _, h, g = input().split()
+    if g != "P":
+        total = total + (float(h) * grade[g])
+        cnt += float(h)
+result = total / cnt
+print(result)
