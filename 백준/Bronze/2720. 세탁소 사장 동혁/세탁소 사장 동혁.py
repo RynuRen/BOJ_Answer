87483636a2f2f22020b8eb0fea222ecf04263ae6
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-05-18 12:04:17 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -2,8 +2,7 @@ coin = [25, 10, 5, 1]
 t = int(input())
 for _ in range(t):
     c = int(input())
-    change = [0, 0, 0, 0]
-    for i in range(4):
-        change[i] = c // coin[i]
-        c %= coin[i]
-    print(*change)
+    for i in coin:
+        print(c//i, end=' ')
+        c %= i
+    print()
