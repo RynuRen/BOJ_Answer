6b08fd02d27b0ac61c22f38a7ee632e2a9901c45
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-05-31 12:56:25 +0900

    [Bronze III] Title: 대지, Time: 92 ms, Memory: 35196 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,18 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-xmax, xmin = -10000, 10000
-ymax, ymin = -10000, 10000
+xlist = []
+ylist = []
 for _ in range(n):
     x, y = map(int, input().split())
-    if x > xmax:
-        xmax = x
-    if x < xmin:
-        xmin = x
-    if y > ymax:
-        ymax = y
-    if y < ymin:
-        ymin = y
-if n == 1:
-    area = 0
-else:
-    area = (xmax - xmin) * (ymax - ymin)
+    xlist.append(x)
+    ylist.append(y)
+area = (max(xlist) - min(xlist)) * (max(ylist) - min(ylist))
 print(area)
