879866a288ea5e901f047624891b545b3684fd9b
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-07-22 11:49:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,8 +2,5 @@ def solution(n, arr1, arr2):
     answer = []
     for i in zip(arr1, arr2):
         binmap = format((i[0] | i[1]), 'b').zfill(n)
-        tmp = ''
-        for j in binmap:
-            tmp += '#' if int(j) else ' '
-        answer.append(tmp)
+        answer.append(''.join(['#' if int(j) else ' ' for j in binmap]))
     return answer
