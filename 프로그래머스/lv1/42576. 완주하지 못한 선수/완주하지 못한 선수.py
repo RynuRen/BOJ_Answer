44fe38cd80f58fcc882b4b0f45b909a79e03b037
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-07-24 22:08:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.67 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(participant, completion):
+    dic = dict(Counter(completion))
+    for p in participant:
+        try:
+            if dic[p] == 0:
+                return p
+            dic[p] -= 1
+        except KeyError:
+            return p
