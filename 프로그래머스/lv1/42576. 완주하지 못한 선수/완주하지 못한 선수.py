7cb8586a717bbfe8f9dadb1fee26f9821222fef8
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-07-24 22:11:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.14 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,15 @@
 from collections import Counter
 
+# def solution(participant, completion):
+#     dic = dict(Counter(completion))
+#     for p in participant:
+#         try:
+#             if dic[p] == 0:
+#                 return p
+#             dic[p] -= 1
+#         except KeyError:
+#             return p
+
 def solution(participant, completion):
-    dic = dict(Counter(completion))
-    for p in participant:
-        try:
-            if dic[p] == 0:
-                return p
-            dic[p] -= 1
-        except KeyError:
-            return p
+    dic = Counter(participant) - Counter(completion)
+    return list(dic)[0]
