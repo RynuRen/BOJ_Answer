28e7827d8d2d403b23c40817415266129d498ca4
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-07-27 20:12:25 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,6 +1,5 @@
 def solution(array, commands):
     answer = []
-    for c in commands:
-        i, j, k = c
+    for i, j, k in commands:
         answer.append(sorted(array[i-1:j])[k-1])
     return answer
