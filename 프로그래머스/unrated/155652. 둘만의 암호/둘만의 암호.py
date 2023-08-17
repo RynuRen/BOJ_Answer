2c0bc9d6cf0c641804e2e21ee72d7eba5e075985
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-08-17 21:11:46 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -2,9 +2,8 @@ from string import ascii_lowercase
 
 def solution(s, skip, index):
     answer = ''
-    alpha = set(ascii_lowercase) - set(skip)
-    alpha_dic = {k:v for v, k in enumerate(sorted(list(alpha)))}
-    alpha_rev = {k:v for v, k in alpha_dic.items()}
+    alpha = sorted(set(ascii_lowercase) - set(skip))
+    alpha_dic = {k:v for v, k in enumerate(alpha)}
     for a in s:
-        answer += alpha_rev[(alpha_dic[a] + index) % len(alpha)]
+        answer += alpha[(alpha_dic[a] + index) % len(alpha)]
     return answer
