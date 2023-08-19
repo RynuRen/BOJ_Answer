5cce0d8475720db0bf89dbad57bbd975d7624e4d
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-08-19 21:10:11 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,9 +1,5 @@
-import sys, decimal
+import sys
 input = sys.stdin.readline
-
-context = decimal.getcontext()
-context.rounding = decimal.ROUND_HALF_UP
-
 t = int(input())
 for i in range(t):
     nums = list(map(int, input().split()))
@@ -12,5 +8,4 @@ for i in range(t):
     for score in nums[1:]:
         if score > avg:
             cnt += 1
-    result = cnt/nums[0]*100
-    print(f"{round(result, 3):.3f}%")
+    print(f"{cnt/nums[0]*100:.3f}%")
