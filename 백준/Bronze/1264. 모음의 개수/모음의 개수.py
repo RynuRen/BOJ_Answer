982d19c660a3d30c0fabe04086bc3a82fa5ebe93
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-09-04 20:41:38 +0900

    [Bronze IV] Title: 모음의 개수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.py
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -1,9 +1,2 @@
-from collections import Counter
-
-a = ['a', 'e', 'i', 'o', 'u']
-while True:
-    s = input()
-    if s == '#':
-        break
-    c = Counter(s.lower())
-    print(sum([c[i] for i in a]))
+while (s:=input()) != '#':
+    print(sum(map(s.lower().count, 'aeiou')))
