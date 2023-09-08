7079399a1db1805b207379d4a53c0e2a4deb9b8d
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-09-08 14:29:15 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -3,12 +3,12 @@ import re
 def solution(dartResult):
     answer = []
     bonus = {"S":1, "D":2, "T":3}
-    rs = re.findall(r'\d+[SDT][*#]*', dartResult)
+    rs = re.findall(r'(\d+)([SDT])([*#]*)', dartResult)
     for r in rs:
-        p = re.search(r'\d+', r).group()
-        b = bonus[re.search(r'[SDT]', r).group()]
-        o = -1 if '#' in r else 1
+        p = r[0]
+        b = bonus[r[1]]
+        o = -1 if '#' in r[2] else 1
         answer.append(int(p)**b*o)
-        if '*' in r:
-            answer = answer[:-2] + [i * 2 for i in answer[-2:]]
+        if '*' in r[2]:
+            answer[-2:] = [i * 2 for i in answer[-2:]]
     return sum(answer)
