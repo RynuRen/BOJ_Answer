e382d2cc08cd98130b9b1c1556b0cd3bd815d32b
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-09-12 13:13:39 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -1,8 +1,10 @@
 def solution(arr):
-    cnt = -1
+    cnt = 0
     _arr = []
-    while arr != _arr:
+    while True:
         _arr = [i // 2 if i >= 50 and i % 2 == 0 else i * 2 + 1 if i < 50 and i % 2 != 0 else i for i in arr]
+        if arr == _arr:
+            break
         cnt += 1
         arr, _arr = _arr, arr
     return cnt
