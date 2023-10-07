e689dff9dc4f220603a83f0f67d6990858dfbbe4
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-10-07 13:55:50 +0900

    [unrated] Title: 배열 만들기 2, Time: 30.24 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,7 +1,6 @@
 def solution(l, r):
     answer = []
-    cond = [set(["0", "5"]), set("0"), set("5")]
     for i in range(l, r + 1):
-        if set(str(i)) in cond:
+        if not set(str(i)) - set(["0", "5"]):
             answer.append(i)
     return answer if answer else [-1]
