27225839335f95a371c13bb4140dd3bbad10fbb0
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-10-13 13:28:08 +0900

    [level 1] Title: 덧칠하기, Time: 1.12 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -2,7 +2,7 @@ def solution(n, m, section):
     cnt = 0
     tmp = 0
     for i in section:
-        if tmp <= i:
-            tmp = i + m
-            cnt +=1
+        if tmp < i:
+            tmp = i + m - 1
+            cnt += 1
     return cnt
