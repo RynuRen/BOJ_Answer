c0f951b127522fb3bec8c8cb76b5ae0869f32be0
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-11-07 13:28:34 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,32 +1,38 @@
+class Pad():
+    def __init__(self):
+        self.answer = ''
+        self.left = (3, 0)
+        self.right = (3, 2)
+    
+    def mv_left(self, coo):
+        self.left = coo
+        self.answer += 'L'
+    
+    def mv_right(self, coo):
+        self.right = coo
+        self.answer += 'R'
+
 def solution(numbers, hand):
-    answer = ''
-    left = (3, 0)
-    right = (3, 2)
+    pad = Pad()
 
     for i in numbers:
         if i == 0:
             i = 11
         coo = ((i - 1) // 3, (i - 1) % 3)
         if i in [1, 4, 7]:
-            left = coo
-            answer += 'L'
+            pad.mv_left(coo)
         elif i in [3, 6, 9]:
-            right = coo
-            answer += 'R'
+            pad.mv_right(coo)
         else:
-            len_l = abs(left[0] - coo[0]) + abs(left[1] - coo[1])
-            len_r = abs(right[0] - coo[0]) + abs(right[1] - coo[1])
+            len_l = abs(pad.left[0] - coo[0]) + abs(pad.left[1] - coo[1])
+            len_r = abs(pad.right[0] - coo[0]) + abs(pad.right[1] - coo[1])
             if len_l == len_r:
                 if hand == 'left':
-                    left = coo
-                    answer += 'L'
+                    pad.mv_left(coo)
                 else:
-                    right = coo
-                    answer += 'R'
+                    pad.mv_right(coo)
             elif len_l < len_r:
-                left = coo
-                answer += 'L'
+                pad.mv_left(coo)
             else:
-                right = coo
-                answer += 'R'
-    return answer
+                pad.mv_right(coo)
+    return pad.answer
