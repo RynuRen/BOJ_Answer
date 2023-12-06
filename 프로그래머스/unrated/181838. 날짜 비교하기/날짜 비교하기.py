71863b3571c73aa3278a11979acacc3da5361839
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-12-06 12:52:23 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,2 +1,3 @@
 def solution(date1, date2):
-    return int(int(''.join(map(str, date1))) < int(''.join(map(str, date2))))
+    return int(date1 < date2)
+    # return int(int(''.join(map(str, date1))) < int(''.join(map(str, date2))))
