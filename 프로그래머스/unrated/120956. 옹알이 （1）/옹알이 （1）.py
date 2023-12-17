ce01b489bf89809f9b2d9d5ef445ef4d1640f1a0
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-12-17 17:16:50 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,10 +1,19 @@
+# def solution(babbling):
+#     answer = 0
+#     words = ["aya", "ye", "woo", "ma"]
+#     for i in babbling:
+#         for j in words:
+#             i = i.replace(j, " ")
+#             if not i.strip():
+#                 answer += 1
+#                 break
+#     return answer
+import re
+
 def solution(babbling):
     answer = 0
-    words = ["aya", "ye", "woo", "ma"]
-    for i in babbling:
-        for j in words:
-            i = i.replace(j, " ")
-            if not i.strip():
-                answer += 1
-                break
+    regex = re.compile('^(aya|ye|woo|ma)+$')
+    for w in babbling:
+        if regex.match(w):
+            answer += 1
     return answer
