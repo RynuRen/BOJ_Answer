66a868cfaae472a0331b0f8c8aee30b084ed6312
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2023-12-28 13:34:19 +0900

    [level unrated] Title: OX퀴즈, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.py
@@ -1,8 +1,22 @@
+# def solution(quiz):
+#     answer = []
+#     for q in quiz:
+#         q = q.split("=")
+#         if eval(q[0].strip()) == int(q[1].strip()):
+#             answer.append("O")
+#         else:
+#             answer.append("X")
+#     return answer
 def solution(quiz):
     answer = []
     for q in quiz:
-        q = q.split("=")
-        if eval(q[0].strip()) == int(q[1].strip()):
+        q = q.split(" = ")
+        f, op, s = q[0].split()
+        if op == "+":
+            rst = int(f) + int(s)
+        else:
+            rst = int(f) - int(s)
+        if rst == int(q[1]):
             answer.append("O")
         else:
             answer.append("X")
