172eb5704b2a04d5c2cfa874f734e46b8252ef92
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-01-12 15:48:08 +0900

    [level Lv.0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/Lv.0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,10 +1,15 @@
+# def solution(numbers):
+#     dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+#     answer = ""
+#     ch = ""
+#     for i in numbers:
+#         ch += i
+#         if dic.get(ch):
+#             answer += dic[ch]
+#             ch = ""
+#     return int(answer)
+
 def solution(numbers):
-    dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
-    answer = ""
-    ch = ""
-    for i in numbers:
-        ch += i
-        if dic.get(ch):
-            answer += dic[ch]
-            ch = ""
-    return int(answer)
+    for n, s in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(s, str(n))
+    return int(numbers)
