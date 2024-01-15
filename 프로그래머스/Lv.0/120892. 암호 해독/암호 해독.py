05209af5d8465f455afc6dd3f74eb24f89b657ae
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-01-15 12:58:28 +0900

    [level Lv.0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/Lv.0/120892. 암호 해독/암호 해독.py
@@ -1,5 +1,8 @@
+# def solution(cipher, code):
+#     answer = ''
+#     for i in range(1, len(cipher) // code + 1):
+#         answer += cipher[i * code - 1]
+#     return answer
+
 def solution(cipher, code):
-    answer = ''
-    for i in range(1, len(cipher) // code + 1):
-        answer += cipher[i * code - 1]
-    return answer
+    return cipher[code - 1::code]
