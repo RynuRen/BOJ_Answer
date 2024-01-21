ab999e98e84c35a56d2484e8db64dfcdc8c8c62b
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-01-21 12:05:55 +0900

    [level 0] Title: 가까운 수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,4 +1,8 @@
+# def solution(array, n):
+#     array.sort()
+#     s = [abs(i - n) for i in array]
+#     return array[s.index(min(s))]
+
 def solution(array, n):
-    array.sort()
-    s = [abs(i - n) for i in array]
-    return array[s.index(min(s))]
+    array.sort(key=lambda x: (abs(x - n), x))
+    return array[0]
