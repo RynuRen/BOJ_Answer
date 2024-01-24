fe47e9113488579f6c25d89d8e3ddc27aab388b7
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-01-24 12:39:52 +0900

    [level 0] Title: k의 개수, Time: 9.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,2 +1,5 @@
+# def solution(i, j, k):
+#     return ''.join(map(str, [*range(i, j + 1)])).count(str(k))
+
 def solution(i, j, k):
-    return ''.join(map(str, [*range(i, j + 1)])).count(str(k))
+    return sum([str(l).count(str(k)) for l in range(i, j + 1)])
