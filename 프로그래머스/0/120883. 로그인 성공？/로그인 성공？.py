72d2474feceb49c552af1f74d85dc3b37bbf3db7
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-01-27 21:04:05 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,10 +1,20 @@
+# def solution(id_pw, db):
+#     ndb = {}
+#     for id, pw in db:
+#         ndb[id] = pw
+#     i, p = id_pw
+#     if ndb.get(i):
+#         if ndb[i] == p:
+#             return "login"
+#         else:
+#             return "wrong pw"
+#     else:
+#         return "fail"
+
 def solution(id_pw, db):
-    ndb = {}
-    for id, pw in db:
-        ndb[id] = pw
-    i, p = id_pw
-    if ndb.get(i):
-        if ndb[i] == p:
+    ndb = dict(db)
+    if ndb.get(id_pw[0]):
+        if ndb[id_pw[0]] == id_pw[1]:
             return "login"
         else:
             return "wrong pw"
