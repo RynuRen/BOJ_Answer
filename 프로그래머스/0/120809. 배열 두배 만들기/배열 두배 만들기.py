722ed0182ff1c702b870a7c6e69a2ebcdd007d56
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-02-15 12:27:50 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,2 +1,6 @@
+# def solution(numbers):
+#     return list(map(lambda x: x * 2, numbers))
+
+# map과 lambda를 같이 쓰면 list comprehension보다 느림
 def solution(numbers):
-    return list(map(lambda x: x * 2, numbers))
+    return [n * 2 for n in numbers]
