b9f747ce2fd94b8eb59249c1880a64d3de8e7a54
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-02-29 12:44:32 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,6 +1,9 @@
-from collections import deque
+# from collections import deque
+
+# def solution(num_list):
+#     answer = deque(num_list)
+#     answer.reverse()
+#     return list(answer)
 
 def solution(num_list):
-    answer = deque(num_list)
-    answer.reverse()
-    return list(answer)
+    return num_list[::-1]
