7297e97b8b0e42fa67a1a2668a00a79b235f7a3a
Jisub Kwak <46422392+RynuRen@users.noreply.github.com>
2024-06-12 22:31:51 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,8 +1,14 @@
-from itertools import permutations
+# from itertools import permutations
+
+# def solution(spell, dic):
+#     perm = ["".join(i) for i in permutations(spell, len(spell))]
+#     for i in dic:
+#         if i in perm:
+#             return 1
+#     return 2
 
 def solution(spell, dic):
-    perm = ["".join(i) for i in permutations(spell, len(spell))]
     for i in dic:
-        if i in perm:
+        if not set(spell) - set(i):
             return 1
     return 2
